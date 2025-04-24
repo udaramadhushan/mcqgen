ec43e21b1ee73988a18292ff44aa815bd7ded190
Udara Madhushan <Udaram188@gmail.com>
2025-04-24 09:49:37 -0400

    adding new files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools  import find_packages, setup
+
+setup(
+
+    name='mcqgenerator',
+    version='0.0.1',
+    author='udara madhushan',
+    install_requires = ['openai', 'langchain', 'streamlit', 'python-dotenv', 'PyPDF2'],
+    packages = find_packages()
+
+
+)
